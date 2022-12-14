3801f8f6da5af537a58fbd6bd1275c8b77a25068
DexGam <peti.cseri65@gmail.com>
2022-12-14 18:00:40 +0100

    Responsive update

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,12 +14,12 @@ function Footer() {
       y: [null, -8, 0],
       pathLength: [0, 1, 0],
       opacity: [null, 1, 0],
-      transition: { delay: 0.1, type: 'spring', duration: 2, bounce: 0.6}
+      transition: { delay: 0.1, type: 'spring', duration: 2, bounce: 0.6 }
     },
     hidden: {
       y: 0,
       pathLength: 0,
-      transition: { duration: 0.1}
+      transition: { duration: 0.1 }
     }
   }
 
@@ -50,13 +50,13 @@ function Footer() {
 
   return (
     <>
+      <svg id="morph" viewBox="0 0 900 300" width="900" height="300" xmlns="http://www.w3.org/2000/svg" version="1.1">
+        <rect x="0" y="0" width="900" height="300" fill="#FFFFFF" strokeOpacity="0.0"></rect>
+        <path class="morph"
+          d="M0 236L21.5 230.2C43 224.3 86 212.7 128.8 208.2C171.7 203.7 214.3 206.3 257.2 215.7C300 225 343 241 385.8 243C428.7 245 471.3 233 514.2 230.5C557 228 600 235 642.8 230.2C685.7 225.3 728.3 208.7 771.2 201.5C814 194.3 857 196.7 878.5 197.8L900 199L900 301L878.5 301C857 301 814 301 771.2 301C728.3 301 685.7 301 642.8 301C600 301 557 301 514.2 301C471.3 301 428.7 301 385.8 301C343 301 300 301 257.2 301C214.3 301 171.7 301 128.8 301C86 301 43 301 21.5 301L0 301Z"
+          fill="#000000" strokeLinecap="round" strokeLinejoin="miter"></path>
+      </svg>
       <footer>
-        <svg id="morph" viewBox="0 0 900 300" width="900" height="300" xmlns="http://www.w3.org/2000/svg" version="1.1">
-          <rect x="0" y="0" width="900" height="300" fill="#FFFFFF"></rect>
-          <path class="morph"
-            d="M0 236L21.5 230.2C43 224.3 86 212.7 128.8 208.2C171.7 203.7 214.3 206.3 257.2 215.7C300 225 343 241 385.8 243C428.7 245 471.3 233 514.2 230.5C557 228 600 235 642.8 230.2C685.7 225.3 728.3 208.7 771.2 201.5C814 194.3 857 196.7 878.5 197.8L900 199L900 301L878.5 301C857 301 814 301 771.2 301C728.3 301 685.7 301 642.8 301C600 301 557 301 514.2 301C471.3 301 428.7 301 385.8 301C343 301 300 301 257.2 301C214.3 301 171.7 301 128.8 301C86 301 43 301 21.5 301L0 301Z"
-            fill="#000000" strokeLinecap="round" strokeLinejoin="miter"></path>
-        </svg>
         <div className="footer-content">
           <h3>Feel Free To Message Me On LinkedIn</h3>
           <address>
@@ -72,17 +72,17 @@ function Footer() {
           <div ref={ref} className="arrowUp">
             <motion.svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" >
               <motion.path d="m 50.5 71 l 0 -60 m -23 19.5 l 23 -19 m 23 19 l -23 -19" fill="transparent"
-                  strokeWidth="7"
-                  stroke="white"
-                  strokeLinecap="round"
-                  initial={{pathLength: 0, opacity: 0}}
-                  animate={arrowVisible ? arrowVariants.visible : arrowVariants.notvisible}
-                />
+                strokeWidth="7"
+                stroke="white"
+                strokeLinecap="round"
+                initial={{ pathLength: 0, opacity: 0 }}
+                animate={arrowVisible ? arrowVariants.visible : arrowVariants.notvisible}
+              />
             </motion.svg>
             <motion.h2
-              initial={{opacity: 0, y: 0}}
-              transition={{delay: 0.3, type: 'spring', duration: 2, bounce: .7}}
-              whileInView={{opacity: [null, 1, 0], times: [0, 0.1, 0.5]}}
+              initial={{ opacity: 0, y: 0 }}
+              transition={{ delay: 0.3, type: 'spring', duration: 2, bounce: .7 }}
+              whileInView={{ opacity: [null, 1, 0], times: [0, 0.1, 0.5] }}
             >
               Click
             </motion.h2>
